a2c46f2012c4e1fef304ab7eb4d8bcac1ceb592f
MinhCuongIT97 <minhcuongit97@gmail.com>
2019-05-25 12:02:11 +0700

    Done

---
+++ Source/QL_QuanAn_3Layer_1Tier/QL_QuanAn_3Layer_1Tier/UI/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace QL_QuanAn_3Layer_1Tier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbClose = new System.Windows.Forms.RadioButton();
+            this.rbOpen = new System.Windows.Forms.RadioButton();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtOwner = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgvMonAn = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMonAn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(185, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hệ Thống Quản Lý Quán Ăn";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QL_QuanAn_3Layer_1Tier.Properties.Resources.logo_mon_an;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbClose);
+            this.groupBox1.Controls.Add(this.rbOpen);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.txtOwner);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(118, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(423, 147);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông Tin Quán Ăn";
+            // 
+            // rbClose
+            // 
+            this.rbClose.AutoSize = true;
+            this.rbClose.Location = new System.Drawing.Point(197, 119);
+            this.rbClose.Name = "rbClose";
+            this.rbClose.Size = new System.Drawing.Size(109, 17);
+            this.rbClose.TabIndex = 5;
+            this.rbClose.TabStop = true;
+            this.rbClose.Text = "Ngừng hoạt động";
+            this.rbClose.UseVisualStyleBackColor = true;
+            // 
+            // rbOpen
+            // 
+            this.rbOpen.AutoSize = true;
+            this.rbOpen.Location = new System.Drawing.Point(88, 119);
+            this.rbOpen.Name = "rbOpen";
+            this.rbOpen.Size = new System.Drawing.Size(103, 17);
+            this.rbOpen.TabIndex = 4;
+            this.rbOpen.TabStop = true;
+            this.rbOpen.Text = "Đang hoạt động";
+            this.rbOpen.UseVisualStyleBackColor = true;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(87, 92);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(330, 20);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(87, 67);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(330, 20);
+            this.txtAddress.TabIndex = 2;
+            // 
+            // txtOwner
+            // 
+            this.txtOwner.Location = new System.Drawing.Point(87, 42);
+            this.txtOwner.Name = "txtOwner";
+            this.txtOwner.Size = new System.Drawing.Size(330, 20);
+            this.txtOwner.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(87, 17);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(330, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Hoạt Động:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Số Điện Thoại:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Địa Chỉ:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Chủ sở hữu:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên Quán Ăn:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(116, 211);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(89, 38);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(211, 211);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(89, 38);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Cập Nhật";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(306, 211);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(89, 38);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dgvMonAn
+            // 
+            this.dgvMonAn.AllowUserToAddRows = false;
+            this.dgvMonAn.AllowUserToDeleteRows = false;
+            this.dgvMonAn.AllowUserToResizeRows = false;
+            this.dgvMonAn.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgvMonAn.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMonAn.Location = new System.Drawing.Point(12, 255);
+            this.dgvMonAn.Name = "dgvMonAn";
+            this.dgvMonAn.ReadOnly = true;
+            this.dgvMonAn.RowHeadersVisible = false;
+            this.dgvMonAn.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMonAn.Size = new System.Drawing.Size(529, 209);
+            this.dgvMonAn.TabIndex = 9;
+            this.dgvMonAn.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMonAn_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(553, 476);
+            this.Controls.Add(this.dgvMonAn);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hệ Thống Quản Lý Địa Điểm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMonAn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbClose;
+        private System.Windows.Forms.RadioButton rbOpen;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtOwner;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgvMonAn;
+    }
+}
+
---
+++ Source/QL_QuanAn_3Layer_1Tier/QL_QuanAn_3Layer_1Tier/UI/Form1.cs
@@ -0,0 +1,182 @@
+﻿using QL_QuanAn_3Layer_1Tier.BUS;
+using QL_QuanAn_3Layer_1Tier.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_QuanAn_3Layer_1Tier
+{
+    public partial class Form1 : Form
+    {
+
+        #region Properties
+        /// <summary>
+        /// Đối tượng lưu trữ các nghiệp vụ của quán ăn
+        /// </summary>
+        QuanAnBUS quanAnBUS = new QuanAnBUS();
+
+        /// <summary>
+        ///Biến lưu trữ giá trị ID quán ăn hiện tại
+        /// </summary>
+        int currIDQuanAn = -1;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Hàm dựng mặc định
+        /// </summary>
+        public Form1()
+        {
+            //Phương thức khởi tại mặc định
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Events
+        /// <summary>
+        /// Tải dữ liệu vào grid khi Form được load lên
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.dgvMonAn.DataSource = quanAnBUS.LoadData().Tables[0].DefaultView;
+            this.dgvMonAn.Columns[0].Visible = false;
+        }
+        /// <summary>
+        /// Xử lý thêm quán ăn
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            bool IsOpen = this.rbOpen.Checked;
+            QuanAn quanAn = new QuanAn() {
+                Name = this.txtName.Text,
+                Owner = this.txtOwner.Text,
+                Address = this.txtAddress.Text,
+                Phone = this.txtPhone.Text,
+                IsOpen = IsOpen
+            };
+            if (quanAnBUS.Insert(quanAn))
+            {
+                MessageBox.Show("Thêm thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.dgvMonAn.DataSource = quanAnBUS.LoadData().Tables[0].DefaultView;
+                clearAllTextbox();
+            }
+            else
+            {
+                MessageBox.Show("Thêm thất bại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        /// <summary>
+        /// Xử lý cập nhật quán ăn
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn không?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                bool IsOpen = this.rbOpen.Checked;
+                QuanAn quanAn = new QuanAn()
+                {
+                    Id = this.currIDQuanAn,
+                    Name = this.txtName.Text,
+                    Owner = this.txtOwner.Text,
+                    Address = this.txtAddress.Text,
+                    Phone = this.txtPhone.Text,
+                    IsOpen = IsOpen
+                };
+                if (quanAnBUS.Update(quanAn))
+                {
+                    MessageBox.Show("Cập nhật thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.dgvMonAn.DataSource = quanAnBUS.LoadData().Tables[0].DefaultView;
+                    clearAllTextbox();
+                }
+                else
+                {
+                    MessageBox.Show("Cập nhật thất bại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        /// <summary>
+        /// Xử lý xóa quán ăn
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn không?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                bool IsOpen = this.rbOpen.Checked;
+                QuanAn quanAn = new QuanAn()
+                {
+                    Id = this.currIDQuanAn,
+                    Name = this.txtName.Text,
+                    Owner = this.txtOwner.Text,
+                    Address = this.txtAddress.Text,
+                    Phone = this.txtPhone.Text,
+                    IsOpen = IsOpen
+                };
+                if (quanAnBUS.Delete(quanAn))
+                {
+                    MessageBox.Show("Xóa thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.dgvMonAn.DataSource = quanAnBUS.LoadData().Tables[0].DefaultView;
+                    clearAllTextbox();
+                }
+                else
+                {
+                    MessageBox.Show("Xóa thất bại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        /// <summary>
+        /// Xử lý binding thông tin lên các field
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dgvMonAn_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            this.currIDQuanAn = (int)this.dgvMonAn.Rows[e.RowIndex].Cells["Id"].Value;
+            this.txtName.Text = this.dgvMonAn.Rows[e.RowIndex].Cells["Name"].Value.ToString();
+            this.txtOwner.Text = this.dgvMonAn.Rows[e.RowIndex].Cells["Owner"].Value.ToString();
+            this.txtAddress.Text = this.dgvMonAn.Rows[e.RowIndex].Cells["Address"].Value.ToString();
+            this.txtPhone.Text = this.dgvMonAn.Rows[e.RowIndex].Cells["Phone"].Value.ToString();
+            bool check = (bool)this.dgvMonAn.Rows[e.RowIndex].Cells["IsOpen"].Value;
+            if (check == true)
+            {
+                this.rbOpen.Checked = true;
+            }
+            else
+            {
+                this.rbClose.Checked = true;
+            };
+        }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Xử lý xóa trắng các textbox
+        /// </summary>
+        private void clearAllTextbox()
+        {
+            this.txtAddress.Clear();
+            this.txtName.Clear();
+            this.txtOwner.Clear();
+            this.txtPhone.Clear();
+            this.rbOpen.Checked = false;
+            this.rbClose.Checked = false;
+        }
+        #endregion
+
+
+    }
+}
